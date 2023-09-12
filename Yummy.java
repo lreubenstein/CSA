0ba3f3d5219abe76eb1f9f404b5507ff0381d8e0
lreubenstein <lreubenstein@billericak12.com>
2023-09-12 10:21:40 -0400

    yummy commit

---
+++ Yummy.java
@@ -0,0 +1,10 @@
+public class Yummy {
+    public static void main(String [] args){
+        System.out.println("Yummy");
+        chocolate();       //call the chocolate method
+    }
+    public static void chocolate(){
+        System.out.print("Chocolate ");
+        System.out.println("Cake");
+    }
+}
